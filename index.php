90ce464f5239c6338eb2f607fcf41d15a2280791
cathy2008 <836609283@qq.com>
2015-07-24 11:05:29 +0800

    www
    wwww

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        div{
+            width: 400px;
+            height: 300px;
+            position: absolute;
+            top: 0px;
+            bottom: 0px;
+            left: 0px;
+            right: 0px;
+            margin: auto auto;
+            background-color: #333333;
+            border-radius: 10px;
+            text-align: center;
+            line-height: 300px;
+            font-size: 3em;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div></div>
+    <div>我叫cathy</div>
+    <div>liooo</div>
+</body>
+</html>
+<?php
+  echo "asdads";
+?>
