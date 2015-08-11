f69e6c85ac597f8d3ed212a39b741fb88858372c
cathy2008 <836609283@qq.com>
2015-08-11 11:21:22 +0800

    check_bookbus

--- bus/php/non_get/check_bookbus.php
+++ bus/php/non_get/check_bookbus.php
@@ -9,41 +9,45 @@ if (isset ( $_SESSION ['emp_number'] ) && isset ( $_SESSION ['Company'] ) && iss
 	$emp_num = $_SESSION ['emp_number'];
 	$db = new DB ();
 	// 记录本周的起始日期
-	$currentdate = strtotime ( date ( 'Y-m-d', time () ) );
-	$currentweek = date ( 'w', time () );
-	switch ($currentweek) {
-		case 1 :
-			break;
-		case 2 :
-			$currentdate -= 1 * 24 * 60 * 60;
-			break;
-		case 3 :
-			$currentdate -= 2 * 24 * 60 * 60;
-			break;
-		case 4 :
-			$currentdate -= 3 * 24 * 60 * 60;
-			break;
-		case 5 :
-			$currentdate -= 4 * 24 * 60 * 60;
-			break;
-		case 6 :
-			$currentdate -= 5 * 24 * 60 * 60;
-			break;
-		case 7 :
-			$currentdate -= 6 * 24 * 60 * 60;
-			break;
-		default :
-			break;
-	}
-	$from = date ( 'Y-m-d', $currentdate ) . " 00:00:00"; // 查询的起始日期
+	/*
+	 * $currentdate = strtotime ( date ( 'Y-m-d', time () ) );
+	 * $currentweek = date ( 'w', time () );
+	 * switch ($currentweek) {
+	 * case 1 :
+	 * break;
+	 * case 2 :
+	 * $currentdate -= 1 * 24 * 60 * 60;
+	 * break;
+	 * case 3 :
+	 * $currentdate -= 2 * 24 * 60 * 60;
+	 * break;
+	 * case 4 :
+	 * $currentdate -= 3 * 24 * 60 * 60;
+	 * break;
+	 * case 5 :
+	 * $currentdate -= 4 * 24 * 60 * 60;
+	 * break;
+	 * case 6 :
+	 * $currentdate -= 5 * 24 * 60 * 60;
+	 * break;
+	 * case 7 :
+	 * $currentdate -= 6 * 24 * 60 * 60;
+	 * break;
+	 * default :
+	 * break;
+	 * }
+	 * $from = date ( 'Y-m-d', $currentdate ) . " 00:00:00"; // 查询的起始日期
+	 */
 	/*
 	 * ****************预约查看*********************
 	 */
 	// 查找当天之后的预约记录
+	$from = date ( 'Y-m-d', time () ) . " 00:00:00";
+// 	echo $from;die;
 	$sql_check = "select FStopID as FStop,FRDate,FRTime from t_hs_overwork_reserv where FNumber='{$_SESSION['emp_number']}' and FRDate>='{$from}' order by 'FRDate' desc";
-// 	echo $sql_check;die;
+	// echo $sql_check;die;
 	$res_check = $db->execsql ( $sql_check );
-// 	var_dump($res_check);die;
+	// var_dump($res_check);die;
 	$num = count ( $res_check );
 	if ($num) {
 		for($i = 0; $i < $num; $i ++) {
@@ -54,9 +58,9 @@ if (isset ( $_SESSION ['emp_number'] ) && isset ( $_SESSION ['Company'] ) && iss
 			$data = explode ( ' ', $res_check [$i] ['FRDate'] );
 			// var_dump($data);
 			$res_check [$i] ['FRDate'] = $data [0];
-			if ($res_check[$i]['FRTime']==NULL){
-				$res_check[$i]['FRTime']='无加班时间项';
-			} 
+			if ($res_check [$i] ['FRTime'] == NULL) {
+				$res_check [$i] ['FRTime'] = '无加班时间项';
+			}
 		}
 	}
 	// 将查到的公司信息与职员的预约记录以JSON格式输出
@@ -65,7 +69,7 @@ if (isset ( $_SESSION ['emp_number'] ) && isset ( $_SESSION ['Company'] ) && iss
 			'section' => $_SESSION ['Section'],
 			'check' => $res_check 
 	);
-// 	var_dump($check_data);die;
+	// var_dump($check_data);die;
 	$checkjson = json_encode ( $check_data );
 	echo $checkjson;
 }
